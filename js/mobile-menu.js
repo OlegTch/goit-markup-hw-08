f78844ed22331d708456c72ef039b0f9d0a05aed
OlegTch <Olgard984@gmail.com>
2021-06-17 01:10:51 +0300

    Добавляет адаптивную графику

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,13 +1,12 @@
 (() => {
-    const menuBtnRef = document.querySelector('[data-menu-button]');
-    const menuBtnRefClose = document.querySelector('[data-menu-button-close]');
-    const mobileMenuRef = document.querySelector('[data-menu]');
-    const functionButton = () => {
-        // const expanded = 
-        // menuBtnRef.getAttribute('aria-expanded') === 'true' || false; 
-        // menuBtnRef.classList.toggle('is-open'); 
-        // menuBtnRefClose.classList.toggle('is-open'); 
-        // menuBtnRef.setAttribute('aria-expanded', !expanded); 
-        mobileMenuRef.classList.toggle('is-open');
-    }; menuBtnRef.addEventListener('click', functionButton); menuBtnRefClose.addEventListener('click', functionButton);
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const menuBtnRefClose = document.querySelector('[data-menu-button-close]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
+
+  const functionButton = () => {
+    mobileMenuRef.classList.toggle('is-open');
+  };
+
+  menuBtnRef.addEventListener('click', functionButton);
+  menuBtnRefClose.addEventListener('click', functionButton);
 })();
